59c5b4cdac4af8490ccd7f38c225255bdad77a13
thomas <thliang01@gmail.com>
2022-03-29 23:34:19 +0800

    feat: add widgetd for line chart

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ st.title("Hello World")
 st.button('Click me')
 
 st.file_uploader("Pick a file")
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
