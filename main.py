1af831319eb21b7cff2dc117dcefa712b01282c1
thomas <thliang01@gmail.com>
2022-03-30 21:02:29 +0800

    feat: add widgetd for DataFrame

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
